740a77906372719aa4c33adc9dca6642e64f356d
nonamebesty <160272703+nonamebesty@users.noreply.github.com>
2024-12-05 01:15:34 +0530

    Update info.py

--- info.py
+++ info.py
@@ -20,33 +20,33 @@ def is_enabled(value, default):
 #main variables
 API_ID = int(environ.get('API_ID', '1923471'))
 API_HASH = environ.get('API_HASH', 'fcdc178451cd234e63faefd38895c991')
-BOT_TOKEN = environ.get('BOT_TOKEN', "7814413831:AAGRr16DhSmAtxXM1XnIBCAKHBGlGArqQgs")
+BOT_TOKEN = environ.get('BOT_TOKEN', "6023399726:AAH0hkqGnwGnUX0-DFQ4CXF_7gihNI2SsTo")
 
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '880087645').split()]
 USERNAME = environ.get('USERNAME', 'https://telegram.me/JAsuran2p0')
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002396553917'))
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001966055101'))
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001188070894 -100169224324 -1002461730224 -1002223699552 -1002397109795 -1001775437494 -1001685364040 -1001274359769').split()]
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://mvideo:mvideo@cluster0.bpj54.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DATABASE_URI2 = environ.get('DATABASE_URI2', "mongodb+srv://mvideo:mvideo@cluster0.bpj54.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DATABASE_NAME = environ.get('DATABASE_NAME', "asuranj1")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://video:video@cluster0.gp0rn.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_URI2 = environ.get('DATABASE_URI2', "mongodb+srv://video:video@cluster0.gp0rn.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_NAME = environ.get('DATABASE_NAME', "asuranj")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'AutofilterBot')
-LOG_API_CHANNEL = int(environ.get('LOG_API_CHANNEL', '-1002396553917'))
+LOG_API_CHANNEL = int(environ.get('LOG_API_CHANNEL', '-1001966055101'))
 QR_CODE = environ.get('QR_CODE', 'https://envs.sh/Cps.jpg')
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
 
 #this vars is for when heroku or koyeb acc get banned, then change this vars as your file to link bot name
-BIN_CHANNEL = int(environ.get('BIN_CHANNEL', '-1002396553917'))
-URL = environ.get('URL', 'https://neutral-lynnet-dappa-159965a6.koyeb.app/')
+BIN_CHANNEL = int(environ.get('BIN_CHANNEL', '-1001966055101'))
+URL = environ.get('URL', '')
 
 # verify system vars
 IS_VERIFY = is_enabled('IS_VERIFY', True)
-LOG_VR_CHANNEL = int(environ.get('LOG_VR_CHANNEL', '-1002396553917'))
+LOG_VR_CHANNEL = int(environ.get('LOG_VR_CHANNEL', '-1001966055101'))
 TUTORIAL = environ.get("TUTORIAL", "https://t.me/demoshort")
 TUTORIAL2 = environ.get("TUTORIAL2", "https://t.me/demoshort")
 TUTORIAL3 = environ.get("TUTORIAL3", "https://t.me/demoshort")
 VERIFY_IMG = environ.get("VERIFY_IMG", "https://envs.sh/Cpl.jpg")
-SHORTENER_API = environ.get("SHORTENER_API", "52d270ec96e971ba3c93464277f5c0393f053287")
-SHORTENER_WEBSITE = environ.get("SHORTENER_WEBSITE", "primeurl.in")
+SHORTENER_API = environ.get("SHORTENER_API", "e5a0df550303e31db72c8924e40e706976a2adb2")
+SHORTENER_WEBSITE = environ.get("SHORTENER_WEBSITE", "Modijiurl.com")
 SHORTENER_API2 = environ.get("SHORTENER_API2", "")
 SHORTENER_WEBSITE2 = environ.get("SHORTENER_WEBSITE2", "")
 SHORTENER_API3 = environ.get("SHORTENER_API3", "")
@@ -57,9 +57,10 @@ THREE_VERIFY_GAP = int(environ.get('THREE_VERIFY_GAP', "21600"))
 # languages search
 LANGUAGES = ["hindi", "english", "telugu", "tamil", "kannada", "malayalam"]
 
-auth_channel = environ.get('AUTH_CHANNEL', '-1002316896807')
+auth_channel = environ.get('AUTH_CHANNEL', '-1002092476960')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
-SUPPORT_GROUP = int(environ.get('SUPPORT_GROUP', '-1002133392581'))
+support_group = int(environ.get('SUPPORT_GROUP')
+SUPPORT_GROUP = int(support_group) if support_group and id_pattern.search(support_group) else None
 
 # bot settings
 AUTO_FILTER = is_enabled('AUTO_FILTER', True)
